da580784c35ecf1fa3513d64499f10090bcf78e1
julian <julianbustospondal@gmail.com>
2026-06-25 17:51:17 -0300

    lectormp3

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -12,11 +12,11 @@ else
 {
     using (FileStream fs = new FileStream(rutaCancion, FileMode.Open))
     {
-        Encoding enc = Encoding.GetEncoding("latin1");
+        Encoding enc = Encoding.Latin1;
         fs.Seek(-128, SeekOrigin.End);
 
         byte[] buffer = new byte[128];
-        fs.Read(buffer, 0, 128);
+        fs.ReadExactly(buffer, 0, 128);
         string identificador = enc.GetString(buffer, 0, 3);
         if (identificador != "TAG")
         {
@@ -25,10 +25,10 @@ else
         }
         else
         {
-            string titulo = enc.GetString(buffer, 3, 30).TrimEnd('\0');
-            string artista = enc.GetString(buffer, 33, 30).TrimEnd('\0');
-            string album = enc.GetString(buffer, 63, 30).TrimEnd('\0');
-            string año = enc.GetString(buffer, 93, 4).TrimEnd('\0');
+            string titulo = enc.GetString(buffer, 3, 30).TrimEnd('\0', ' ');
+            string artista = enc.GetString(buffer, 33, 30).TrimEnd('\0', ' ');
+            string album = enc.GetString(buffer, 63, 30).TrimEnd('\0', ' ');
+            string año = enc.GetString(buffer, 93, 4).TrimEnd('\0', ' ');
 
             Console.WriteLine($"Título: {titulo}");
             Console.WriteLine($"Artista: {artista}");
